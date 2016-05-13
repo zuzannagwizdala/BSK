db5df0854c56cfa8c0b5466e9330bf2f284692ae
zgwizdala94 <z.gwizdala94@gmail.com>
2016-05-13 23:54:45 +0200

    Logowanie

--- BSK/Controllers/LogInLogicController.cs
+++ BSK/Controllers/LogInLogicController.cs
@@ -152,4 +152,3 @@ namespace BSK.Controllers
         }
     }
 }
-}
---
+++ BSK/Scripts/OurScripts/LogIn.js
@@ -0,0 +1,48 @@
+﻿loading = false;
+
+    form = {
+        login: '',
+        password: ''
+    };
+    
+
+    function sendForm() {
+        console.log("weszlo3");
+        loading = true;
+        errorMessage = '';
+        if (document.getElementById('text') && document.getElementById('pwd')) {     //&& form.role
+            console.log("weszlo");
+            $http.post('~/Controllers/LogInLogic', {
+                login: document.getElementById('text'),
+                password: document.getElementById('pwd'),
+                role: ''
+            })
+                .then(function (res) {
+                    res.data.Login = form.login;
+                    res.data.Role = { Name: form.role.Name, Id: form.role.Id };
+                    UserService.setLoggedUser(res);
+                    $location.url('/');
+                    loading = false;
+                })
+                .catch(function (err) {
+                    errorMessage = err.data.Message || 'Niepoprawne dane!';
+                    loading = false;
+                });
+        } else if (form.login && form.password) {
+            console.log("weszlo2");
+            $http.post('~/Controllers/LogInLogic', {
+                login: form.login,
+                password: form.password,
+                role: null
+            })
+                .then(function (res) {
+                    roles = res.data;
+                    form.role = roles[0];
+                    loading = false;
+                })
+                .catch(function (err) {
+                    errorMessage = err.data.Message || 'Niepoprawne dane!';
+                    loading = false;
+                });
+        }
+    };
