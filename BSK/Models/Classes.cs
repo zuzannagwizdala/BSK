1d5aee9c88f05428b6d2097cc2bc4521d548982a
paulinabukowska <paulinabukowska94@gmail.com>
2016-05-29 15:27:00 +0200

    usuniete debuggery i zmienna adres ustawiona jako pusty string

--- BSK/Models/Classes.cs
+++ BSK/Models/Classes.cs
@@ -236,7 +236,7 @@ namespace BSK.Models
     [DbConfigurationType(typeof(NpgsqlConfiguration))]
     public class DB : DbContext
     {
-        static String ConnectionString = "Server=192.168.0.35;User ID=postgres;Password=paulina;Database=Ksiegarnia;syncnotification=false;port=5432";
+        static String ConnectionString = "Server=localhost;User ID=postgres;Password=paulina;Database=Ksiegarnia;syncnotification=false;port=5432";
         public DB()
             : base(new NpgsqlConnection(ConnectionString), true) { }
         public DbSet<Autor> Autorzy { get; set; }
